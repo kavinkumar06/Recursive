368936c0a75260d921eb5c24087fedaa8d01b8f6
kavinkumar06 <kavinm09@gmail.com>
2017-08-01 11:20:27 +0530

    Create Recursive.java

---
+++ Recursive.java
@@ -0,0 +1,19 @@
+# Recursive
+import java.util.Scanner;
+public class Recursive {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a,b;
+			System.out.println("Enter the first number");
+			Scanner s=new Scanner(System.in);
+			a=s.nextInt();
+			System.out.println("Enter the second number");
+			b=s.nextInt();
+			
+	System.out.println(a!=b);  // TODO code application logic here
+	    }
+      }
